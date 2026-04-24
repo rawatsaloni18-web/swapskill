3e7511e5e849417f0c046a056578ec5266457c6c
rawatsaloni18-web <rawatsaloni18@gmail.com>
2026-04-24 23:54:10 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,13 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "skill_exchange");
+$host = "shuttle.proxy.rlwy.net";
+$user = "root";
+$pass = "UTGKGqdiKyIgicZWBOoWFoZWbWCKsRTB";
+$db   = "railway";
+$port = "43025";
 
-if(!$conn){
-    die("Connection failed: " . mysqli_connect_error());
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
